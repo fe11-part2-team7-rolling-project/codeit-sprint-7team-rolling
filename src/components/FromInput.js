53ae2a6bf239ea7eb5ec2b65f601ae2d3fd8866c
Stemplun <dnfl311@pusan.ac.kr>
2024-10-25 22:31:34 +0900

    feat: 입력 값이 없을 때 에러 처리 추가
    - 입력된 값이 없을 때 에러 메시지를 표시하기 위해 error 상태(useState) 추가
    - Input 필드가 focus out 되었을 때 유효성 검사를 통해 에러 메시지를 표시

--- src/components/FromInput.js
+++ src/components/FromInput.js
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 
 const FromInput = () => {
   const [value, setValue] = useState("");
+  const [error, SetError] = useState(false);
 
+  const handleBlur = () => {
+    if (!value) {
+      SetError(true);
+    }
+  };
   return (
     <div>
       <label>From.</label>
@@ -13,8 +19,11 @@ const FromInput = () => {
           setValue(e.target.value);
           SetError(false);
         }}
+        onBlur={handleBlur}
         placeholder="이름을 입력해 주세요."
+        className={error ? "input-error" : ""}
       />
+      {error && <span className="input-error">값을 입력해 주세요.</span>}
     </div>
   );
 };
