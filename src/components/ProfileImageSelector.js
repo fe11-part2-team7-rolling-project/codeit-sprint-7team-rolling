911feab3d173ad55324b49dab657583ec13f5c7a
Stemplun <dnfl311@pusan.ac.kr>
2024-10-25 22:45:26 +0900

    feat: 프로필 이미지 선택 기능 구현 완료
    - 프로필 이미지 선택 기능 구현
    - 사용자가 제공된 여러 이미지 중 하나를 클릭해 선택할 수 있음
    - 기본 프로필 이미지는 default_profile.svg로 설정
    - 선택된 이미지는 미리보기로 표시되며, 선택된 이미지는 테두리로 강조
    - 이미지 선택 UI는 flex 레이아웃을 사용하여 정렬

--- src/components/ProfileImageSelector.js
+++ src/components/ProfileImageSelector.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
+import profileImg01 from "../img/profile_img_01.svg";
+import profileImg02 from "../img/profile_img_02.svg";
+import profileImg03 from "../img/profile_img_03.svg";
+import defaultProfile from "../img/default_profile.svg";
 
 const ProfileImageSelector = () => {
-  const [selectedImage, setSelectedImage] = useState("");
+  const [selectedImage, setSelectedImage] = useState(defaultProfile);
+  const imagePaths = [profileImg01, profileImg02, profileImg03];
+
+  const handleImageSelect = (image) => {
+    setSelectedImage(image);
+  };
 
   return (
     <div>
@@ -14,7 +23,23 @@ const ProfileImageSelector = () => {
         />
       </div>
       <p>프로필 이미지를 선택해 주세요!</p>
-      <div style={{ display: "flex", gap: "10px" }}></div>
+      <div style={{ display: "flex", gap: "10px" }}>
+        {imagePaths.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Profile ${index + 1}`}
+            onClick={() => handleImageSelect(image)}
+            style={{
+              width: "50px",
+              height: "50px",
+              borderRadius: "50%",
+              cursor: "pointer",
+              border: selectedImage === image ? "2px solid #000" : "none",
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 };
