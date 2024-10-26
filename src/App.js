de375d4cc8ca54e34fcb62434620a4004d116326
Stemplun <dnfl311@pusan.ac.kr>
2024-10-27 08:55:40 +0900

    feat: App.js에 라우트 추가
    - App.js에 MessagePage 라우트를 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import PostIdPage from './pages/PostIdPage';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PostIdPage from "./pages/PostIdPage";
+import MessagePage from "./pages/messagePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/post/:Id" element={<PostIdPage />} />
+        <Route path="/post/:Id/message" element={<MessagePage />} />
       </Routes>
     </BrowserRouter>
   );
