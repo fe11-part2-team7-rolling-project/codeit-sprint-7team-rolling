4d0695f2c11cf196b6378e99b00362345cccff5c
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-10-26 20:48:51 +0900

    Feat: axios를 이용한 api 연결 후 배경 이미지 생성

--- src/pages/PostIdPage.js
+++ src/pages/PostIdPage.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axiosInstance from '../axiosInstance';
 
 function PostIdPage() {
+  const [images, setImages] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await axiosInstance.get('/background-images/');
+      console.log(response.data.imageUrls); // 요청이 성공적으로 완료되면 데이터를 출력
+      setImages(response.data.imageUrls);
+    } catch (error) {
+      console.error('Error fetching data:', error); // 에러 발생 시 처리
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="bg-blue100 text-gray300 p-4">
-      <div className="text-3xl font-bold">To. Ashley Kim</div>
+    <div className="w-full h-screen">
+      <div className="bg-white text-black w-full h-[52px]">
+        <div className="flex flex-row items-center px-6 w-full h-full text-[18px] leading-[26px] font-regular">
+          To. Ashley Kim
+        </div>
+      </div>
+      <img src={images[3]} alt="배경 이미지" className="w-full h-full" />
     </div>
   );
 }
