c1fb2aa67cfe004c5f74aa31aedd077bbef6206d
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-10-28 15:56:22 +0900

    Remove: ReactionIconBtn.js 파일 삭제 및 불필요한 코드 제거

--- src/pages/PostIdPage.js
+++ src/pages/PostIdPage.js
@@ -6,7 +6,6 @@ import Message from '../components/Message';
 
 function PostIdPage() {
   const [images, setImages] = useState([]);
-  const [profile, setProfile] = useState([]);
 
   async function fetchBackgroundImageData() {
     try {
@@ -18,19 +17,8 @@ function PostIdPage() {
     }
   }
 
-  async function fetchProfileImageData() {
-    try {
-      const response = await axiosInstance.get('/profile-images/');
-      console.log(response.data.imageUrls);
-      setProfile(response.data.imageUrls);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
-
   useEffect(() => {
     fetchBackgroundImageData();
-    fetchProfileImageData();
   }, []);
 
   return (
@@ -67,38 +55,6 @@ function PostIdPage() {
             >
               +
             </button>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 items-center justify-center p-4 z-10">
-          <div className="w-full h-full min-h-[230px] bg-white flex items-center justify-center rounded-[16px] shadow-lg">
-            <button
-              type="button"
-              className="w-[56px] h-[56px] p-4 bg-gray500  rounded-full text-[24px] text-white flex flex-row justify-center items-center"
-            >
-              +
-            </button>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 items-center justify-center p-4 z-10">
-          <div className="w-full h-full min-h-[230px] bg-white flex items-center justify-center rounded-[16px] shadow-lg">
-            <div className="flex flex-row gap-[16px]">
-              <img
-                src={`${profile[1]}`}
-                alt="프로필 이미지"
-                className="w-[56px] h-[56px] rounded-full"
-              />
-              <div className="flex flex-col gap-[6px]">
-                <div className="flex flex-row gap-[6px] items-center">
-                  <div className="text-[18px] font-regular leading-[28px] text-black">
-                    From.
-                  </div>
-                  <div className="text-[16px] font-bold leading-[26px] text-black">
-                    김동훈
-                  </div>
-                </div>
-                <div>친구</div>
-              </div>
-            </div>
           </div>
         </div>
         <Message />
