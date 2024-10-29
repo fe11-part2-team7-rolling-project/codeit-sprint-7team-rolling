4763ed27374d0c1708f0ad3bd5ee793daf96f705
Stemplun <dnfl311@pusan.ac.kr>
2024-10-29 11:40:46 +0900

    feat: CreateButton에서 prop을 useState로 관리하여 데이터 전달 기능 추가
    - MessagePage에서 FromInput, ProfileImageSelector, RelationSelector, TextEditor, FontSelector의 값을 상태로 관리
    - 각 컴포넌트의 입력값을 CreateButton에 prop으로 전달하도록 수정
    - CreateButton에서 입력된 값들을 기반으로 POST 요청을 보낼 수 있도록 구현

--- src/pages/messagePage.js
+++ src/pages/messagePage.js
@@ -9,6 +9,9 @@ import CreateButton from "../components/CreateButton";
 function MessagePage() {
   const [from, setFrom] = useState("");
   const [content, setContent] = useState("");
+  const [relation, setRelation] = useState("지인"); // 기본값 설정
+  const [font, setFont] = useState("Noto Sans"); // 기본 폰트 설정
+  const [profileImageURL, setProfileImageURL] = useState(""); // 기본 프로필 이미지 설정
 
   return (
     <div style={{ padding: "20px", maxWidth: "600px", margin: "auto" }}>
@@ -18,19 +21,25 @@ function MessagePage() {
       <FromInput onInputChange={setFrom} />
 
       {/* Profile Image Selector */}
-      <ProfileImageSelector />
+      <ProfileImageSelector onSelectImage={setProfileImageURL} />
 
       {/* Relation Selector */}
-      <RelationSelector />
+      <RelationSelector onSelectRelation={setRelation} />
 
       {/* Text Editor */}
       <TextEditor onContentChange={setContent} />
 
       {/* Font Selector */}
-      <FontSelector />
+      <FontSelector onSelectFont={setFont} />
 
       {/* Create Button */}
-      <CreateButton from={from} content={content} />
+      <CreateButton
+        from={from}
+        content={content}
+        relation={relation}
+        font={font}
+        profileImageURL={profileImageURL}
+      />
     </div>
   );
 }
