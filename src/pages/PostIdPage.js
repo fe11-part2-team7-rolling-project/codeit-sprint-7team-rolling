6190d2f883fea16132d7bfcf1c83130e17327637
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-10-30 03:05:12 +0900

    Feat:react-toastify라이브러리를 이용한 toast메시지 추가

--- src/pages/PostIdPage.js
+++ src/pages/PostIdPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axiosInstance from '../axiosInstance';
 import Reactions from '../components/Reactions';
 import Message from '../components/Message';
@@ -8,22 +10,30 @@ import Share from '../components/Share';
 function PostIdPage() {
   const [images, setImages] = useState([]);
 
-  async function fetchBackgroundImageData() {
-    try {
-      const response = await axiosInstance.get('/background-images/');
-      console.log(response.data.imageUrls);
-      setImages(response.data.imageUrls);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
-
   useEffect(() => {
+    async function fetchBackgroundImageData() {
+      try {
+        const response = await axiosInstance.get('/background-images/');
+        console.log(response.data.imageUrls);
+        setImages(response.data.imageUrls);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
     fetchBackgroundImageData();
   }, []);
 
   return (
     <div className="w-full h-full">
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        draggable
+        className="p-4"
+      />
       <div className="sticky top-0 z-20">
         <div className="bg-white text-black w-full h-[52px] border-b border-gray200">
           <div className="flex flex-row items-center px-6 w-full h-full text-[18px] leading-[26px] font-regular">
@@ -47,7 +57,7 @@ function PostIdPage() {
           <div className="w-full h-full min-h-[230px] bg-white flex items-center justify-center rounded-[16px] shadow-lg">
             <Link
               to="/post/1/message"
-              className="w-[56px] h-[56px] bg-gray500  rounded-full text-[24px] text-white flex justify-center items-center"
+              className="w-[56px] h-[56px] bg-gray500 rounded-full text-[24px] text-white flex justify-center items-center"
             >
               +
             </Link>
