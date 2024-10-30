6193f10f68634663d8524fdd1dda0032b9d065eb
JunHee <z1top159@gmail.com>
2024-10-30 17:10:31 +0900

    Feat: Card컴포넌트 분리

---
+++ src/components/CardItem.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+function CardItem({ item, nav, colorClassMap }) {
+  return (
+    <button
+      type="button"
+      onClick={() => nav(`/post/${item.id}`)}
+      className={`relative flex flex-col items-start justify-start border-black/[.10] border-[1px] gap-3 pt-[30px] pl-[24px] rounded-2xl min-w-[208px] h-[232px] min-[376px]:min-w-[275px] min-[376px]:min-h-[260px] shadow-md ${
+        item.backgroundImageURL
+          ? "text-white"
+          : colorClassMap[item.backgroundColor]
+      }`}
+      style={{
+        backgroundImage: item.backgroundImageURL
+          ? `url(${item.backgroundImageURL})`
+          : undefined,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      {item.backgroundImageURL && (
+        <div className="absolute inset-0 bg-black opacity-50 rounded-2xl" />
+      )}
+      <div className="relative z-10 text-start flex flex-col gap-3">
+        {/* 받는 사람 */}
+        <div
+          className={`font-bold text-lg leading-7 min-[376px]:text-2xl ${
+            item.backgroundImageURL ? "text-white" : "text-gray900"
+          }`}
+        >
+          To. {item.name}
+        </div>
+
+        {/* 프로필 이미지 섹션 */}
+        <div className="flex -space-x-3">
+          {item.recentMessages.slice(0, 3).map((message) => (
+            <img
+              key={message.id}
+              src={message.profileImageURL}
+              alt={message.sender}
+              className="w-7 h-7 rounded-full border-2 border-white"
+            />
+          ))}
+          {item.messageCount > 3 && (
+            <span className="w-8 h-7 rounded-[30px] bg-white font-regular text-gray500 text-xs flex justify-center items-center">
+              +{item.messageCount - 3}
+            </span>
+          )}
+        </div>
+        <div className="text-sm font-regular min-[376px]:text-base">
+          <span className="font-bold">{item.messageCount}</span>명이 작성했어요!
+        </div>
+
+        {/* 리액션 섹션 */}
+        <div className="flex w-[162px] space-x-1 border-black/[.12] border-t-[1px] pt-[17px] mt-[30px] min-[376px]:w-[227px] min-[376px]:space-x-1.5">
+          {item.topReactions.map((reaction) => (
+            <div
+              key={reaction.id}
+              className="flex items-center justify-center space-x-1 bg-black/50 w-[53px] h-[32px] rounded-full text-white text-sm min-[376px]:text-base min-[376px]:w-[65px] min-[376px]:h-[36px]"
+            >
+              <span>{reaction.emoji}</span>
+              <span>{reaction.count}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </button>
+  );
+}
+
+export default CardItem;
--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import getList from "../listApi";
+import arrowLeft from "../assets/arrow_left.svg";
+import arrowRight from "../assets/arrow_right.svg";
+import CardItem from "./CardItem";
 
 function ListCard({ type }) {
   const [list, setList] = useState([]);
@@ -49,59 +52,22 @@ function ListCard({ type }) {
           <button
             type="button"
             onClick={() => setCurrentPage(currentPage - 1)}
-            className="absolute left-0 z-10 bg-white rounded-full w-10 h-10 flex items-center justify-center shadow-md hover:bg-gray-100"
+            className="absolute top-[110px] left-[-20px] z-10 bg-white rounded-full w-10 h-10 flex items-center justify-center shadow-md hover:bg-gray-100"
           >
-            <span className="text-lg font-bold">&lt;</span>
+            <span>
+              <img src={arrowLeft} alt="왼쪽 화살표" />
+            </span>
           </button>
         )}
 
         {/* 현재 페이지에 해당하는 카드들만 표시 */}
         {currentCards.map((item) => (
-          <button
-            type="button"
+          <CardItem
             key={item.id}
-            onClick={() => nav(`/post/${item.id}`)}
-            className={`relative flex flex-col items-start justify-start border-black/[.10] border-[1px] gap-5 pt-[30px] pl-[24px] rounded-2xl min-w-[208px] h-[232px] min-[376px]:min-w-[275px] min-[376px]:min-h-[260px] shadow-md ${
-              item.backgroundImageURL
-                ? "text-white"
-                : colorClassMap[item.backgroundColor]
-            }`}
-            style={{
-              backgroundImage: item.backgroundImageURL
-                ? `url(${item.backgroundImageURL})`
-                : undefined,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          >
-            {item.backgroundImageURL && (
-              <div className="absolute inset-0 bg-black opacity-50 rounded-2xl" />
-            )}
-            <div className="relative z-10 text-start">
-              <div className="font-bold text-2xl leading-7">
-                To. {item.name}
-              </div>
-              <div className="flex -space-x-3 mt-2">
-                {item.recentMessages.slice(0, 3).map((message) => (
-                  <img
-                    key={message.id}
-                    src={message.profileImageURL}
-                    alt={message.sender}
-                    className="w-7 h-7 rounded-full border-2 border-white"
-                  />
-                ))}
-                {item.recentMessages.length > 3 && (
-                  <span className="w-8 h-7 rounded-[30px] bg-white text-gray-500 text-xs flex justify-center items-center">
-                    +{item.recentMessages.length - 3}
-                  </span>
-                )}
-              </div>
-              <div className="text-base font-regular mt-2">
-                <span className="font-bold">{item.messageCount}</span>명이
-                작성했어요!
-              </div>
-            </div>
-          </button>
+            item={item}
+            nav={nav}
+            colorClassMap={colorClassMap}
+          />
         ))}
 
         {/* 우측 화살표 버튼 */}
@@ -109,9 +75,11 @@ function ListCard({ type }) {
           <button
             type="button"
             onClick={() => setCurrentPage(currentPage + 1)}
-            className="absolute right-0 z-10 bg-white rounded-full w-10 h-10 flex items-center justify-center shadow-md hover:bg-gray-100"
+            className="absolute top-[110px] right-[-20px] z-10 bg-white rounded-full w-10 h-10 flex items-center justify-center shadow-md border border-gray300"
           >
-            <span className="text-lg font-bold">&gt;</span>
+            <span>
+              <img src={arrowRight} alt="오른쪽 화살표" />
+            </span>
           </button>
         )}
       </div>
@@ -119,51 +87,12 @@ function ListCard({ type }) {
       {/* 모바일 및 태블릿 환경에서 가로 스크롤 */}
       <div className="flex gap-3 overflow-x-auto scrollbar-hide min-[1025px]:hidden min-[376px]:gap-5">
         {list.map((item) => (
-          <button
-            type="button"
+          <CardItem
             key={item.id}
-            onClick={() => nav(`/post/${item.id}`)}
-            className={`relative flex flex-col items-start justify-start border-black/[.10] border-[1px] gap-3 pt-[30px] pl-[24px] rounded-2xl min-w-[208px] h-[232px] min-[376px]:min-w-[275px] min-[376px]:min-h-[260px] shadow-md ${
-              item.backgroundImageURL
-                ? "text-white"
-                : colorClassMap[item.backgroundColor]
-            }`}
-            style={{
-              backgroundImage: item.backgroundImageURL
-                ? `url(${item.backgroundImageURL})`
-                : undefined,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          >
-            {item.backgroundImageURL && (
-              <div className="absolute inset-0 bg-black opacity-50 rounded-2xl" />
-            )}
-            <div className="relative z-10 text-start">
-              <div className="font-bold text-lg leading-7 min-[376px]:text-2xl">
-                To. {item.name}
-              </div>
-              <div className="flex -space-x-3 mt-2">
-                {item.recentMessages.slice(0, 3).map((message) => (
-                  <img
-                    key={message.id}
-                    src={message.profileImageURL}
-                    alt={message.sender}
-                    className="w-7 h-7 rounded-full border-2 border-white"
-                  />
-                ))}
-                {item.recentMessages.length > 3 && (
-                  <span className="w-8 h-7 rounded-[30px] bg-white text-gray-500 text-xs flex justify-center items-center">
-                    +{item.recentMessages.length - 3}
-                  </span>
-                )}
-              </div>
-              <div className="text-sm font-regular mt-2 min-[376px]:text-base">
-                <span className="font-bold">{item.messageCount}</span>명이
-                작성했어요!
-              </div>
-            </div>
-          </button>
+            item={item}
+            nav={nav}
+            colorClassMap={colorClassMap}
+          />
         ))}
       </div>
     </div>
