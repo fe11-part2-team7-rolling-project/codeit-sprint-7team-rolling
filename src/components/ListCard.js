4c3137b04b29681d7e6f0f82d8bf65e8c52db91b
JunHee <z1top159@gmail.com>
2024-10-30 14:37:12 +0900

    Feat: ListCard 배경 이미지 등록 및 배치 수정

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -42,36 +42,53 @@ function ListCard({ type }) {
           type="button"
           key={item.id}
           onClick={() => nav(`/post/${item.id}`)}
-          className={`flex flex-col border-black/[.10] border-[1px] gap-3 pt-[30px] pl-[24px] rounded-2xl min-w-[208px] h-[232px] min-[376px]:min-w-[275px] min-[376px]:min-h-[260px] shadow-md ${
-            colorClassMap[item.backgroundColor]
+          className={`relative flex flex-col items-start justify-start border-black/[.10] border-[1px] gap-3 pt-[30px] pl-[24px] rounded-2xl min-w-[208px] h-[232px] min-[376px]:min-w-[275px] min-[376px]:min-h-[260px] shadow-md ${
+            item.backgroundImageURL
+              ? "text-white"
+              : colorClassMap[item.backgroundColor]
           }`}
+          style={{
+            backgroundImage: item.backgroundImageURL
+              ? `url(${item.backgroundImageURL})`
+              : undefined,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
         >
-          {/* 받는 사람 */}
-          <div className="font-bold text-lg text-gray900 leading-7 min-[376px]:text-2xl min-[376px]:leading-9">
-            To. {item.name}
+          {/* 배경 이미지 오버레이 - 80% 투명도 */}
+          {item.backgroundImageURL && (
+            <div className="absolute inset-0 bg-black opacity-20 rounded-2xl" />
+          )}
+
+          <div className="relative z-10 text-start">
+            {/* 받는 사람 */}
+            <div className="font-bold text-lg leading-7 min-[376px]:text-2xl min-[376px]:leading-9">
+              To. {item.name}
+            </div>
+
+            {/* 프로필 이미지 섹션 */}
+            <div className="flex -space-x-3 mt-2">
+              {item.recentMessages.slice(0, 3).map((message) => (
+                <img
+                  key={message.id}
+                  src={message.profileImageURL}
+                  alt={message.sender}
+                  className="w-7 h-7 rounded-full border-2 border-white"
+                />
+              ))}
+              {item.recentMessages.length > 3 && (
+                <span className="w-8 h-7 rounded-[30px] bg-white text-gray-500 text-xs flex justify-center items-center">
+                  +{item.recentMessages.length - 3}
+                </span>
+              )}
+            </div>
+            <div className="text-sm font-regular mt-2 min-[376px]:text-base">
+              {item.messageCount}명이 작성했어요!
+            </div>
           </div>
 
-          {/* 프로필 이미지 섹션 */}
-          <div className="flex -space-x-3">
-            {item.recentMessages.slice(0, 3).map((message) => (
-              <img
-                key={message.id}
-                src={message.profileImageURL}
-                alt={message.sender}
-                className="w-7 h-7 rounded-full border-2 border-white"
-              />
-            ))}
-            {item.recentMessages.length > 3 && (
-              <span className="w-8 h-7 rounded-[30px] bg-white text-gray500 text-xs flex justify-center items-center">
-                +{item.recentMessages.length - 3}
-              </span>
-            )}
-          </div>
-          <div className="text-sm font-regular text-gray700 min-[376px]:text-base">
-            {item.messageCount}명이 작성했어요!
-          </div>
           {/* 리액션 섹션 */}
-          <div className="flex w-[162px] space-x-1 border-black/[.12] border-t-[1px] pt-[17px] mt-[21px] min-[376px]:w-[227px] min-[376px]:space-x-1.5">
+          <div className="relative z-10 flex w-[162px] space-x-1 border-black/[.12] border-t-[1px] pt-[17px] mt-[21px] min-[376px]:w-[227px] min-[376px]:space-x-1.5">
             {item.topReactions.map((reaction) => (
               <div
                 key={reaction.id}
