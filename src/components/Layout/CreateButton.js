3b06a0785283149fd8ea4a807bffa17bddae42d8
안형석 <hyungsuk12370@gmail.com>
2024-10-31 18:27:18 +0900

    Fix:CreateButton오탈자 수정

--- src/components/Layout/CreateButton.js
+++ src/components/Layout/CreateButton.js
@@ -7,7 +7,7 @@ function CreateButton() {
       to="/post"
       className="text-gray700 border border-gray-400 px-4 py-2 rounded-md"
     >
-      롤링 페이지 만들기
+      롤링 페이퍼 만들기
     </Link>
   );
 }
